ced44b1e3243070ca88bcc064158c6d0bb25ab58
YeetoN420 <111980744+YeetoN420@users.noreply.github.com>
2023-10-17 10:32:37 +0200

    balls

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+
+<h1>
+    HOME
+</h1>
+
+<form method="post" action="/login?/logout" use:enhance>
+
+    <button>LOGOUT</button>
+
+</form>
+
+<h2><a href="http://localhost:5174/sessions">Sessions</a></h2>
